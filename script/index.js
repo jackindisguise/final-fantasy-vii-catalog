55cc3284e5bb6f265f9e28dd8394753be17d3206
jackindisguise <incognitus.me@gmail.com>
2021-04-14 07:45:20 -0500

    feat: Added initial processing script, spreadsheets, and decks.

---
+++ script/index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+let sourceLines = [];
+let targetLines = [];
+let parsedLines = [];
+fs.readFile("in.txt", "utf8", function(err, data){
+	let lines = data.split("\r\n");
+	for(let line of lines){
+		let split = line.split("\t");
+		let source = split[0];
+		let target = split[1];
+		sourceLines.push(source);
+		targetLines.push(target);
+	}
+
+	let sourceLinesRaster = sourceLines.join("\r\n");
+	sourceLinesRaster = sourceLinesRaster.replace(/“([^”]*)”/g, function(a,b){ return `${b.replace(/\r\n/g, " ")}`; });
+	sourceLinesRaster = sourceLinesRaster.replace(/(?:\r\n){2,}/g, function(){ return "\r\n"; });
+
+	let targetLinesRaster = targetLines.join("\r\n");
+	targetLinesRaster = targetLinesRaster.replace(/「([^」]*)」/g, function(a,b){ return `${b.replace(/\r\n/g, "")}`; });
+	targetLinesRaster = targetLinesRaster.replace(/(?:\r\n){2,}/g, function(a){ return "\r\n"; });
+
+	sourceLines = sourceLinesRaster.split("\r\n");
+	targetLines = targetLinesRaster.split("\r\n");
+
+	let formatted = [];
+	for(i=0;i<sourceLines.length;i++) formatted.push(`${sourceLines[i]}\t${targetLines[i]}`);
+	fs.writeFile("out.txt", formatted.join("\r\n"), "utf8", function(err){ });
+});
