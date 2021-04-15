3ab20023a06765f69b6a04847af2ba0700171067
jackindisguise <incognitus.me@gmail.com>
2021-04-15 07:21:44 -0500

    feat: Script now detects when a new dialogue block starts in the middle of a dialogue block and assumes it's the end of the previous block.

--- script/process.js
+++ script/process.js
@@ -13,11 +13,19 @@ fs.readFile("formatted.txt", "utf8", function(err, data){
 	}
 
 	let sourceLinesRaster = sourceLines.join("\r\n");
+	// automatically close open dialogue when it comes upon another open dialogue
+	sourceLinesRaster = sourceLinesRaster.replace(/“([^”]*)\r\n“/g, function(a,b){ return `${b.replace(/\r\n/g, " ")}\r\n“`; });
+	// collapse linebreaks in open dialogue
 	sourceLinesRaster = sourceLinesRaster.replace(/“([^”]*)”/g, function(a,b){ return `${b.replace(/\r\n/g, " ")}`; });
+	// removes double linebreaks
 	sourceLinesRaster = sourceLinesRaster.replace(/(?:\r\n){2,}/g, function(){ return "\r\n"; });
 
 	let targetLinesRaster = targetLines.join("\r\n");
+	// automatically close open dialogue when it comes upon another open dialogue
+	targetLinesRaster = targetLinesRaster.replace(/「([^」]*)\r\n「/g, function(a,b){ return `${b.replace(/\r\n/g, "")}\r\n「`; });
+	// collapse linebreaks in open dialogue
 	targetLinesRaster = targetLinesRaster.replace(/「([^」]*)」/g, function(a,b){ return `${b.replace(/\r\n/g, "")}`; });
+	// removes double linebreaks
 	targetLinesRaster = targetLinesRaster.replace(/(?:\r\n){2,}/g, function(a){ return "\r\n"; });
 
 	sourceLines = sourceLinesRaster.split("\r\n");
