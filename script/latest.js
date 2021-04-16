ff9475b75cc299ecfd82f12476d63765b4a0eabe
jackindisguise <incognitus.me@gmail.com>
2021-04-16 02:30:29 -0500

    feat: Just generates 1 line of HTML to add to the Anki deck.

---
+++ script/latest.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let data = fs.readFileSync("../CHANGELOG.md", "utf8");
+
+// latest version
+let rule = /### \[(.*?)\]\((.*?)\) (.+)/;
+let result = rule.exec(data);
+let version = result[1];
+let link = result[2];
+let date = result[3];
+console.log(`<b>Latest Version:</b> <b><a href="${link}">${version}</a></b> ${date}`);
