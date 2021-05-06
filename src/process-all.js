61c935ef5fb9a69e613ac49098c9b7529cac8e42
jackindisguise <incognitus.me@gmail.com>
2021-05-06 13:25:24 -0500

    feat: Processed lines now add tag data to the end (scene_xx). Also produces a file with all scenes combined.

--- src/process-all.js
+++ src/process-all.js
@@ -17,13 +17,18 @@ Array.prototype.contains = function(item){
 // all lines get stored here to remove duplicates
 let usedJapanese = [];
 
+// save all lines for combination
+let combined = [];
+
 console.log("Processing all scene files:");
 fs.readdir(formatted, function(err, files){
 	if(err) return;
 	for(let file of files){
 		let source = formatted+file;
 		if(source.indexOf(".txt") === -1) continue;
-		let name = file.substring(0,file.length-4);
+		let noext = file.substring(0,file.length-4);
+		let num = new Number(noext.substring(0,2));
+		let name = noext.substring("00 - ".length);
 		let target = processed+file;
 		let data = fs.readFileSync(source, "utf8");
 		let original = data.split("\r\n");
@@ -33,14 +38,17 @@ fs.readdir(formatted, function(err, files){
 		for(let entry of result) {
 			if(usedJapanese.contains(entry.target)) continue; // don't add duplicate japanese lines
 			usedJapanese.push(entry.target); // track japanese lines
-			lines.push(`${entry.source}\t${entry.target}`);
+			let line = `${entry.source}\t${entry.target}\tscene_${num.toString().padStart(2, "0")}`;
+			lines.push(line);
+			combined.push(line)
 		}
 		newLines += lines.length;
 		fs.writeFileSync(target, lines.join("\r\n"), "utf8");
-		console.log(`\t${name}`);
-		console.log(`\t\t${original.length} lines > ${lines.length} lines (${((lines.length / original.length * 100)).toFixed(2)}% used)`)
-		console.log("");
+		console.log(`\t[${num.toString().padStart(2, "0")}] ${name}: ${original.length} lines > ${lines.length} lines (${((lines.length / original.length * 100)).toFixed(2)}% used)`)
 	}
+
+	fs.writeFileSync(processed+"combined.txt", combined.join("\r\n"));
+	console.log(`Generated ${processed}combined.txt`);
 	console.log(`Original Lines: ${originalLines}`);
 	console.log(`New Lines: ${newLines} (${(newLines/originalLines*100).toFixed(2)}% used)`);
 });
