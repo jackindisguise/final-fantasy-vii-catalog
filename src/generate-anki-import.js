05d1864bf16d2942d60942ab406f87dcf48a8e13
jackindisguise <incognitus.me@gmail.com>
2021-05-06 23:48:20 -0500

    fix(script): Added misc. text anki import generator.

--- src/generate-anki-import.js
+++ src/generate-anki-import.js
@@ -1,17 +1,18 @@
 // node includes
 const fs = require("fs");
 
-// local consts
-const inputDir = "../scene/processed/";
-const outputDir = "../anki/scene/";
-let kanjiRule = /([\u3400-\u4DB5\u4E00-\u9FCB\uF900-\uFA6A])/g;
+// generate scene import files
+{
+	// local consts
+	const inputDir = "../scene/processed/";
+	const outputDir = "../anki/scene/";
+	let kanjiRule = /([\u3400-\u4DB5\u4E00-\u9FCB\uF900-\uFA6A])/g;
 
-// save all lines for combination
-let combined = [];
+	// save all lines for combination
+	let combined = [];
 
-console.log("Converting processed scenes to anki imports:");
-fs.readdir(inputDir, function(err, files){
-	if(err) return;
+	console.log("Converting processed scenes to anki imports:");
+	let files = fs.readdirSync(inputDir);
 	for(let file of files){
 		if(file.indexOf(".txt") === -1) continue;
 		if(file === "combined.txt") continue // we'll make our owns
@@ -38,4 +39,43 @@ fs.readdir(inputDir, function(err, files){
 
 	fs.writeFileSync(outputDir+"combined.txt", combined.join("\r\n"));
 	console.log(`Generated ${outputDir}combined.txt`);
-});
+}
+
+console.log("");
+// generate misc. text import files
+{
+	// local consts
+	const inputDir = "../vocabulary/misc/";
+	const outputDir = "../anki/misc/";
+	let kanjiRule = /([\u3400-\u4DB5\u4E00-\u9FCB\uF900-\uFA6A])/g;
+
+	// save all lines for combination
+	let combined = [];
+
+	console.log("Converting misc. text to anki imports:");
+	let files = fs.readdirSync(inputDir);
+	for(let file of files){
+		if(file.indexOf(".txt") === -1) continue;
+		let noext = file.substring(0,file.length-4);
+		let data = fs.readFileSync(inputDir+file, "utf8");
+
+		// split lines
+		let lines = data.split("\r\n");
+		let formatted = [];
+		for(let line of lines) {
+			let split = line.split("\t");
+			let japanese = split[0];
+			let literal = split[2];
+			let english = split[3];
+			let kanji = japanese.match(kanjiRule) || [];
+			let format = `${japanese}\t${english}\t${literal}\t${kanji.join("")}\t${noext}`;
+			formatted.push(format);
+			combined.push(format)
+		}
+		fs.writeFileSync(outputDir+file, formatted.join("\r\n"), "utf8");
+		console.log(`\t${file}`);
+	}
+
+	fs.writeFileSync(outputDir+"combined.txt", combined.join("\r\n"));
+	console.log(`Generated ${outputDir}combined.txt`);
+}
