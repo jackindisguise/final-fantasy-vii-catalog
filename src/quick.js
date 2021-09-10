659075800eb8be8f571d00185b996e2ae495c68d
jackindisguise <incognitus.me@gmail.com>
2021-09-09 20:58:58 -0500

    chore: forgot about this script.

--- src/quick.js
+++ src/quick.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
-const process = require("./process");
+const processScript = require("./process-script");
 
 fs.readFile("formatted.txt", "utf8", function(err, data){
 	let original = data.split("\r\n");
-	let result = process(data);
+	let result = processScript(data);
 	let lines = [];
 	for(let entry of result) lines.push(`${entry.source}\t${entry.target}`);
 	fs.writeFileSync("./processed.txt", lines.join("\r\n"), "utf8");
